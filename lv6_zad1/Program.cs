f3b9cc06932faf5d63d7b1de4ba9f106b3768894
mihovilsimic <mihovil.simic@student.ferit.hr>
2021-12-20 16:38:46 +0100

    project init

---
+++ lv6_zad1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lv6_zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
